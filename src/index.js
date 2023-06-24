6060abc3337a931f4dfdeec9d79f85cb9b97cedf
sabir <sabirovich2014@mail.ru>
2023-06-24 22:27:12 +0400

    commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import reader from './reader.js';
 const iter = (data1, data2) => {
   const key1 = _.keys(data1);
   const key2 = _.keys(data2);
-  const keys = _.sortBy(key1, key2);
-  keys.forEach((key) => {
-    if (typeof data1[key] === 'object') {
-      console.log(key, data1[key]);
-      iter(data1[key]);
+  const keys = _.merge(key1, key2);
+  const sortedKeys = _.sortBy(keys);
+  sortedKeys.forEach((key) => {
+    if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
+      iter(data1[key], data2[key2]);
     } else {
       console.log(key, data1[key]);
     }
