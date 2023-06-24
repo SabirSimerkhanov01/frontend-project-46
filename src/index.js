62454f067f189e28a4414195352c7233f61abaac
sabir <sabirovich2014@mail.ru>
2023-06-24 21:15:54 +0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import lodash from 'lodash';
+import _ from 'lodash';
 import reader from './reader.js';
+
+const iter = (file1, file2) => {
+  const keys = _.keys(file1);
+  keys.forEach((key) => {
+    if (typeof file1[key] === 'object') {
+      console.log(key, file1[key]);
+      iter(file1[key]);
+    } else {
+      console.log(key, file1[key]);
+    }
+  });
+};
 
 const genDiff = (file1, file2) => {
   const data1 = reader(file1);
   const data2 = reader(file2);
-  return data1;
+  return iter(data1, data2);
 };
 
 export default genDiff;
